29cabda6a3d2a30ebf3aaa0aca299dc0275b8057
Namit Agrawal <namit.agrawal20@gmail.com>
2020-07-26 15:19:53 -0500

    Cleaned predictions_file

--- Prediction/predictions_based_on_position.R
+++ Prediction/predictions_based_on_position.R
@@ -8,13 +8,14 @@ library(gbm)
 
 player_2017_salary_metrics<-read.csv("2017_player_salary_and_metrics.csv", stringsAsFactors = FALSE)
 
+MultipleLinearRMSE = c()
+RandomForestRMSE = c()
+BoostingRMSE = c()
 
 Trees <- function(pos_dataset, name){
     pos_dataset[,c("Pos")] <- list(NULL)
     # print(summary(pos_dataset$salaryPercSalaryCap))
     n=dim(pos_dataset)[1]
-    
-    
     set.seed(123)
     ind <- sample(1:n,size=n*0.80,replace = FALSE)
     train = pos_dataset[ind,]
@@ -50,22 +51,53 @@ Trees <- function(pos_dataset, name){
     }
     ilrf = round(sqrt(ilrf/nrow(train)),3) #In-sample RMSE
     olrf = round(sqrt(olrf/nrow(test)),3) #Out-of-sample RMSE
+
+    iirf=which.min(olrf) #Find minimum oos loss
+    therf = rffitv[[iirf]] #Get model which correspond to the minimum out-sample loss
+    therfpred=predict(therf,newdata=test)
+
+    pred_year = data.frame("Year"=test_Year, "Percentage"=therfpred)
+    actualSal_year = data.frame("Year"=test_Year, "Percentage"=test$salaryPercSalaryCap)
+    #Transform the percentages to actual Salary using the salary cap from each year
+    pred_year=transform(pred_year, Salary=ifelse(Year==2010, Percentage*58044000,
+                                                 ifelse(Year==2011, Percentage*58044000,
+                                                        ifelse(Year==2012, Percentage*58044000,
+                                                               ifelse(Year==2013, Percentage*58679000,
+                                                                      ifelse(Year==2014, Percentage*63065000,
+                                                                             ifelse(Year==2015, Percentage*70000000,
+                                                                                    ifelse(Year==2016, Percentage*94143000, Percentage*99093000))))))))
+    actualSal_year = transform(actualSal_year, Salary=ifelse(Year==2010, Percentage*58044000,
+                                                             ifelse(Year==2011, Percentage*58044000,
+                                                                    ifelse(Year==2012, Percentage*58044000,
+                                                                           ifelse(Year==2013, Percentage*58679000,
+                                                                                  ifelse(Year==2014, Percentage*63065000,
+                                                                                         ifelse(Year==2015, Percentage*70000000,
+                                                                                                ifelse(Year==2016, Percentage*94143000, Percentage*99093000))))))))
+    #print(pred_year)
+    #print(actualSal_year)
+    #Calculate RMSE and adjusted R-squared
+    SE=sum(((actualSal_year$Salary)-(pred_year$Salary))^2)
+    RMSE=round(sqrt(SE/nrow(actualSal_year)),3)
+    R2 = 1 - sum(((actualSal_year$Salary)-(pred_year$Salary))^2)/sum(((actualSal_year$Salary)-mean((pred_year$Salary)))^2)
+    adjrsquared = 1 - (1 - R2) * ((dim(test)[1] - 1)/(dim(test)[1]-p))
+    cat(RMSE," ",R2," ",name," ", adjrsquared, "\n")
+    RandomForestRMSE <<- c(RandomForestRMSE, RMSE)
     #Print losses
     # print(cbind(parmrf,ilrf, olrf))
     
-    #Boosting trees
+    # #Boosting trees
     idv = c(4,10) #tree depth
     ntv = c(1000,5000) #number of trees
     lamv=c(.001,.2) #Learning rates
     parmb = expand.grid(idv,ntv,lamv) #Expand the values to get different models
     colnames(parmb) = c('tdepth','ntree','lam')
     # print(parmb)
-    
+
     nset = nrow(parmb) #Number of models
     olb = rep(0,nset) #Out-of-sample loss
     ilb = rep(0,nset) #In-sample loss
     bfitv = vector('list',nset) #List of the estimated models
-    
+
     for(i in 1:nset) {
         # cat('Model ',i,'out of',nset,'\n')
         tempboost = gbm(salaryPercSalaryCap~.,#Formula
@@ -84,44 +116,36 @@ Trees <- function(pos_dataset, name){
     olb = round(sqrt(olb/nrow(test)),3) #In-sample RMSE
     iib=which.min(olb) #Find minimum oos loss
     theb = bfitv[[iib]] #Select the model with minimum oos loss
-    thebpred = predict(theb,newdata=test,n.trees=parmb[iib,2]) #Get the prediction for the validation set
+    thebpred = predict(theb,newdata=test,n.trees=parmb[iib,2])
     pred_year = data.frame("Year"=test_Year, "Percentage"=thebpred)
     actualSal_year = data.frame("Year"=test_Year, "Percentage"=test$salaryPercSalaryCap)
     #Transform the percentages to actual Salary using the salary cap from each year
-    pred_year=transform(pred_year, Salary=ifelse(Year==2010, Percentage*58044000, 
+    pred_year=transform(pred_year, Salary=ifelse(Year==2010, Percentage*58044000,
                                 ifelse(Year==2011, Percentage*58044000,
                                 ifelse(Year==2012, Percentage*58044000,
                                 ifelse(Year==2013, Percentage*58679000,
                                 ifelse(Year==2014, Percentage*63065000,
                                 ifelse(Year==2015, Percentage*70000000,
                                 ifelse(Year==2016, Percentage*94143000, Percentage*99093000))))))))
-    actualSal_year = transform(actualSal_year, Salary=ifelse(Year==2010, Percentage*58044000, 
+    actualSal_year = transform(actualSal_year, Salary=ifelse(Year==2010, Percentage*58044000,
                                                       ifelse(Year==2011, Percentage*58044000,
                                                      ifelse(Year==2012, Percentage*58044000,
                                                     ifelse(Year==2013, Percentage*58679000,
                                                     ifelse(Year==2014, Percentage*63065000,
                                                      ifelse(Year==2015, Percentage*70000000,
                                                     ifelse(Year==2016, Percentage*94143000, Percentage*99093000))))))))
-    #print(pred_year)
-    #print(actualSal_year)
+
+    #Calculate RMSE and adjusted R-squared
     SE=sum(((actualSal_year$Salary)-(pred_year$Salary))^2)
     RMSE=round(sqrt(SE/nrow(actualSal_year)),3)
-    print(RMSE)
+    R2 = 1 - sum(((actualSal_year$Salary)-(pred_year$Salary))^2)/sum(((actualSal_year$Salary)-mean((pred_year$Salary)))^2)
+    adjrsquared = 1 - (1 - R2) * ((dim(test)[1] - 1)/(dim(test)[1]-p))
+    cat(RMSE," ",R2," ",name," ", adjrsquared, "\n")
+    BoostingRMSE <<- c(BoostingRMSE, RMSE)
     #Print losses
     #print(cbind(parmb,olb,ilb))
 
-    
-    #Results no grouping
-    # tdepth ntree   lam     olb        ilb
-    # 1      4  1000 0.001 5275090 5085007.13
-    # 2     10  1000 0.001 5130949 4911225.99
-    # 3      4  5000 0.001 4178061 3840245.47
-    # 4     10  5000 0.001 4031305 3539281.22
-    # 5      4  1000 0.200 4932238 1482678.51
-    # 6     10  1000 0.200 4389668  169447.29
-    # 7      4  5000 0.200 4693875   35362.97
-    # 8     10  5000 0.200 4428822       1.31
-    print(name)
+    #print(name)
 }
 
 Perform_Linear_regression <- function(pos_dataset, name){
@@ -172,13 +196,16 @@ Perform_Linear_regression <- function(pos_dataset, name){
                                                                                                 ifelse(Year==2016, Percentage*94143000, Percentage*99093000))))))))
     #print(pred_year)
     #print(actualSal_year)
+    #Calculate RMSE and adjusted R-squared
     SE=sum(((actualSal_year$Salary)-(pred_year$Salary))^2)
     RMSE=round(sqrt(SE/nrow(actualSal_year)),3)
     print(RMSE)
+    MultipleLinearRMSE <<- c(MultipleLinearRMSE, RMSE)
+    
     
     # MSE <- mean((exp(test$Salary)-exp(y.hat))**2)
     # print(sqrt(MSE))
-    # print(name)
+    print(name)
 
     
     
@@ -196,7 +223,7 @@ cleanPlayerSalary <- function(player_csv){
     #Hot one-encoding for Team
     #player_csv <- dummy_cols(player_csv, select_columns = 'Tm', remove_first_dummy = TRUE)
     #Drop the text and duplicate columns
-    player_csv[,c("X", "player_id", "COLID", "Tm", "firstName", "lastName", "player_Year", "league", "season", "season_end", "season_start", "Player", "team", "Salary")] <- list(NULL)
+    player_csv[,c( "Tm", "Player", "Salary")] <- list(NULL)
     #Subset the dataframe by position
     SG_dataset  <- subset(player_csv, Pos == "SG")
     C_dataset  <- subset(player_csv, Pos == "C")
@@ -210,13 +237,31 @@ cleanPlayerSalary <- function(player_csv){
     Perform_Linear_regression(PF_dataset, "Power Forward")
     Perform_Linear_regression(SF_dataset, "Small Forward")
     Perform_Linear_regression(PG_dataset, "Point Guard")
-    # Trees(player_csv, "Overall")
-    # Trees(SG_dataset, "Shooting Guard")
-    # Trees(C_dataset, "Center")
-    # Trees(PF_dataset, "Power Forward")
-    # Trees(SF_dataset, "Small Forward")
-    # Trees(PG_dataset, "Point Guard")
+    #Perform Random Forest and Boosting
+    Trees(player_csv, "Overall")
+    Trees(SG_dataset, "Shooting Guard")
+    Trees(C_dataset, "Center")
+    Trees(PF_dataset, "Power Forward")
+    Trees(SF_dataset, "Small Forward")
+    Trees(PG_dataset, "Point Guard")
     
+    # print(MultipleLinearRMSE)
+    # print(RandomForestRMSE)
+    # print(BoostingRMSE)
+    
+    #Plot the RMSE for each model
+    barplot(MultipleLinearRMSE, main = "Multiple Linear Regression RMSE of Predicted Salaries",
+            xlab = "Position",
+            ylab = "Salary", 
+            names.arg=c("Overall", "SG", "C", "PF", "SF", "PG"), col = "darkred")
+    barplot(RandomForestRMSE, main = "Random Forest RMSE of Predicted Salaries",
+            xlab = "Position",
+            ylab = "Salary", 
+            names.arg=c("Overall", "SG", "C", "PF", "SF", "PG"), col = "darkred")
+    barplot(BoostingRMSE, main = "Boosting RMSE of Predicted Salaries",
+            xlab = "Position",
+            ylab = "Salary", 
+            names.arg=c("Overall", "SG", "C", "PF", "SF", "PG"), col = "darkred")
     #RMSE CALCULATED USING SALARY CAP PERCENTAGES - OUT OF SAMPLE - Boosting Trees
     # 2852859 - "No Grouping"
     # 3257454 - "Shooting Guard"
@@ -225,107 +270,22 @@ cleanPlayerSalary <- function(player_csv){
     # 3506058 - "Small Forward"
     # 2909815 - "Point Guard"
     
+    #RMSE CALCULATED USING SALARY CAP PERCENTAGES - OUT OF SAMPLE - Random Forest
+    # 3080980 - "Overall"
+    # 3337990 - "Shooting Guard"
+    # 3934175 - "Center"
+    # 2230782 - "Power Forward"
+    # 3625654 - "Small Forward"
+    # 3366156 - "Point Guard"
+    
     #RMSE CALCULATED USING SALARY CAP PERCENTAGES - OUT OF SAMPLE - Multi Linear Regression
     # 3060917 - "No Grouping"
     # 3582381 - "Shooting Guard"
     # 4042490 - "Center"
     # 2285300 - "Power Forward"
     # 3576691 - "Small Forward"
-    # 2621779 - "Point Guard"
+    # 3621779 - "Point Guard"
     
-    
-    
-    
-    #RMSE CALCULATED USING SALARY COLUMN
-    #RSME for different models
-    #Trees Result (Point Guard Boosting)
-    #tdepth ntree   lam     olb         ilb
-    # 1      4  1000 0.001 5506735 4829594.530
-    # 2     10  1000 0.001 5303531 4554384.146
-    # 3      4  5000 0.001 4245805 3353807.654
-    # 4     10  5000 0.001 3938456 2554879.939
-    # 5      4  1000 0.200 4020126   26254.172
-    # 6     10  1000 0.200 3713437    1881.572
-    # 7      4  5000 0.200 4117787       0.000
-    # 8     10  5000 0.200 4171727       0.000
-    #Random Forest(Point Guard)
-    # 1   47   100 4073006 4405797
-    # 2    7   100 4266135 4596289
-    # 3   47   500 4067542 4385697
-    # 4    7   500 4263556 4621702
-    
-    
-    #Trees Result (Small Forward Boosting)
-    # tdepth ntree   lam     olb         ilb
-    # 1      4  1000 0.001 5862465 4994789.269
-    # 2     10  1000 0.001 5705319 4705288.361
-    # 3      4  5000 0.001 4509738 3347267.917
-    # 4     10  5000 0.001 4379000 2530689.765
-    # 5      4  1000 0.200 4192193   21057.625
-    # 6     10  1000 0.200 4688942    1331.720
-    # 7      4  5000 0.200 4931656       0.001
-    # 8     10  5000 0.200 4460381       0.000
-    #Random Forest(Small Forward)
-    # mtry ntree    ilrf    olrf
-    # 1   47   100 4269703 4879056
-    # 2    7   100 4332129 4908045
-    # 3   47   500 4236599 4798365
-    # 4    7   500 4317717 4925752
-    
-    
-    #Trees Result (Power Forward Boosting)
-    # tdepth ntree   lam     olb         ilb
-    # 1      4  1000 0.001 3729822 4959034.839
-    # 2     10  1000 0.001 3609930 4678861.142
-    # 3      4  5000 0.001 3011802 3282727.411
-    # 4     10  5000 0.001 3017894 2532943.570
-    # 5      4  1000 0.200 3867109   35008.983
-    # 6     10  1000 0.200 3773858     897.580
-    # 7      4  5000 0.200 3698884       0.004
-    # 8     10  5000 0.200 3968908       0.000
-    #Random Forest(Power Forward)
-    # mtry ntree    ilrf    olrf
-    # 1   47   100 4146855 3203545
-    # 2    7   100 4238444 3317751
-    # 3   47   500 4121224 3198080
-    # 4    7   500 4252930 3281175
-    
-    
-    #Trees Result (Center Boosting)
-    # tdepth ntree   lam     olb         ilb
-    # 1      4  1000 0.001 6018251 5116536.092
-    # 2     10  1000 0.001 5913308 4820797.738
-    # 3      4  5000 0.001 5171775 3515316.427
-    # 4     10  5000 0.001 5111544 2696009.660
-    # 5      4  1000 0.200 5257364   43840.728
-    # 6     10  1000 0.200 5259646     655.658
-    # 7      4  5000 0.200 4952887       0.003
-    # 8     10  5000 0.200 5115411       0.000
-    #Random Forest(Center)
-    # mtry ntree    ilrf    olrf
-    # 1   47   100 4489011 5328404
-    # 2    7   100 4660426 5470666
-    # 3   47   500 4429075 5287558
-    # 4    7   500 4663364 5473414
-    
-    
-    #Trees Result (Shooting Guard Boosting)
-    # tdepth ntree   lam     olb         ilb
-    # 1      4  1000 0.001 5169419 4841689.688
-    # 2     10  1000 0.001 5039949 4582721.297
-    # 3      4  5000 0.001 4151157 3396990.997
-    # 4     10  5000 0.001 4038905 2632023.530
-    # 5      4  1000 0.200 4291002   41129.921
-    # 6     10  1000 0.200 3976290     315.819
-    # 7      4  5000 0.200 4277914       0.001
-    # 8     10  5000 0.200 3958650       0.000
-    #Random Forest(Shooting Guard)
-    # mtry ntree    ilrf    olrf
-    # 1   47   100 4291789 4442072
-    # 2    7   100 4378622 4542887
-    # 3   47   500 4255561 4377670
-    # 4    7   500 4339349 4500589
-
     return (player_csv)
 }
 
